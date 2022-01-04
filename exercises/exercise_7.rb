056ed750fb47a508159cd8a9c2855129baf1b3cb
Bernadette Abalos <bern.roseabalos@gmail.com>
2022-01-04 20:21:14 +0000

    Implement exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, :numericality => { greater_than_or_equal_to: 40, less_than: 200 }
+  validates :store_id, presence: true
+end 
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, :numericality => { only_integer: true, greater_than: 0}
+end 
+
+puts "Please enter a new store name:"
+print "> "
+new_store = gets.chomp
+
+@store7 = Store.create(name: new_store)
+error_messages = @store7.errors.full_messages
+
+error_messages.each do |error|
+  puts "Error! #{error}"
+end
